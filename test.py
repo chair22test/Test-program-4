97a5ff0a3c44e56eb1776f99f1c8214a5e9336c1
helodrys <luxsanachimplee@gmail.com>
2026-03-06 21:46:25 +0700

    added feature

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import time
+import random
 
 print("Program starting")
 
 for i in range (0,100):
-	print(i)
+	smt = random.randint(0,3)
+	if smt == 2 or smt == 0:
+		(print("Random number salt : " + str(random.randint(0,1000))))
+	if i % 2 == 0:
+		print("Even number: " + str(i))
+	else:
+		print("Odd number: " + str(i))
 	time.sleep(1)
 
 
